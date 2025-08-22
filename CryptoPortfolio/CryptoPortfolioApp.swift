11dd54ecad96d33492d3629751c84e426d4096f6
Mayank Jangid <226301131@gkv.ac.in>
2025-08-22 11:23:13 +0530

    Initial Commit

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by Mayank Jangid on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
